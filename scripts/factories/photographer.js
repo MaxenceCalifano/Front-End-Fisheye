dcb19e38f77037a6e9ff128860a5e90a435bb0dd
Maxence <maxence.califano@gmail.com>
2022-10-06 16:54:54 +0200

    Revert "build: load api data"
    This reverts commit e512b793f8f81f5c1a16a556be046a56821689d8.

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,32 +1,16 @@
 function photographerFactory(data) {
-    const { name, portrait, city, country, tagline, price } = data;
-    console.log('data ', data)
-    const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
+    const { name, portrait } = data;
+
+    const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
-        img.setAttribute("alt", "portrait du photographe")
-
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-
-        const p = document.createElement("p");
-        p.textContent = `${city}, ${country}`;
-
-        const taglineText = document.createElement("p");
-        taglineText.textContent = tagline;
-
-        const priceText = document.createElement("p");
-        priceText.textContent = `${price}€/jour`
-
         article.appendChild(img);
         article.appendChild(h2);
-        article.appendChild(p);
-        article.appendChild(taglineText);
-        article.appendChild(priceText);
         return (article);
     }
     return { name, picture, getUserCardDOM }
