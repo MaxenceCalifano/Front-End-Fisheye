1627eddd58a6af4a8f959af8fe940725b4495214
Maxence <maxence.califano@gmail.com>
2022-10-10 19:18:21 +0200

    build: media factory

---
+++ scripts/factories/mediaFactory.js
@@ -0,0 +1,10 @@
+function mediafactory(media, name) {
+    //if l'objet a une propriété image ou vidéo
+    function getImageCard() {
+        const image = document.createElement('img');
+        image.setAttribute('src', `../../assets/images/Sample Photos/${name}/${media.image}`)
+        console.log(image)
+        return image;
+    }
+    return {getImageCard};
+}
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -9,11 +9,11 @@ async function getPhotographer() {
     .then(res => { photographer = res.photographers, media = res.media})
     .catch(err => console.log('an error occurs', err))
     
-    photographer = photographer.find( element => element.id == photographerID)
-    media.filter( image => image.id == photographerID);
-    console.log(media.filter( image => image.photographerId == photographerID))
+    photographer = photographer.find( element => element.id == photographerID);
+
+    const photographerMedia = media.filter( image => image.photographerId == photographerID);
     
-    return ({photographer : photographer})
+    return ({photographer : photographer, photographerMedia: photographerMedia})
 }
 
     
@@ -28,11 +28,25 @@ async function getPhotographer() {
 
 };
 
+async function displayMedia(photographerMedia, name) {
+    const firstName = name.split(' ')[0].replace('-', ' ');
+
+    console.log(firstName)
+    const mediaSection = document.querySelector("main");
+    photographerMedia.forEach( media => {
+        const mediaModel = mediafactory(media, firstName);
+        const imageCard = mediaModel.getImageCard();
+        mediaSection.appendChild(imageCard);
+    })
+}
 
 async function init() {
     // Récupère les datas des photographes
     const { photographer } = await getPhotographer();
+    const { photographerMedia } = await getPhotographer();
+   //console.log(photographer)
     displayData(photographer);
+    displayMedia(photographerMedia, photographer.name);
 };
 
 init();
