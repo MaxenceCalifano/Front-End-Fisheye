22ba2452260cd6c7e7b8128b93aa33fe8d68204a
Maxence <maxence.califano@gmail.com>
2022-10-10 21:06:39 +0200

    fix: media factory check video

--- scripts/factories/mediaFactory.js
+++ scripts/factories/mediaFactory.js
@@ -1,9 +1,26 @@
 function mediafactory(media, name) {
     //if l'objet a une propriété image ou vidéo
-    function getImageCard() {
-        const image = document.createElement('img');
-        image.setAttribute('src', `../../assets/images/Sample Photos/${name}/${media.image}`)
-        return image;
+    if(media.image) {
+        function getImageCard() {
+            const image = document.createElement('img');
+            image.setAttribute('src', `../../assets/images/Sample Photos/${name}/${media.image}`);
+            return image;
+        }
+    } else if(media.video) {
+        function getImageCard() {
+            const video = document.createElement("video");
+            const source = document.createElement("source");
+
+            video.setAttribute('controls', true);
+            source.setAttribute('src', `../../assets/images/Sample Photos/${name}/${media.video}`)
+
+            video.appendChild(source);
+            console.log(video);
+            return video;
+        }
+    } else {
+        console.error("No file to display");
     }
+    
     return {getImageCard};
 }
